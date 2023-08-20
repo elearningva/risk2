7884b21a45293221d8e3d20738e34d21731deab0
elearningva <77228344+elearningva@users.noreply.github.com>
2023-08-20 19:36:22 +0800

    risk

---
+++ story_content/user.js
@@ -0,0 +1,247 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6o5ffgl4bOT":
+        Script1();
+        break;
+      case "6TcYjcpYD9V":
+        Script2();
+        break;
+      case "5cTR4M3REBt":
+        Script3();
+        break;
+      case "5dlAocVXqZY":
+        Script4();
+        break;
+      case "5o6ys0MwIsB":
+        Script5();
+        break;
+      case "6IRRJA2VnIG":
+        Script6();
+        break;
+      case "6FvX4x9zkVC":
+        Script7();
+        break;
+      case "6FQpqqGrJch":
+        Script8();
+        break;
+      case "5VJe3WQN3iH":
+        Script9();
+        break;
+      case "6OUo77Ji5TE":
+        Script10();
+        break;
+  }
+}
+
+function Script1()
+{
+  var x = document.getElementsByClassName("scrollarea-area");
+x[0].scrollTop = 0;
+var height = x[0].scrollHeight - x[0].offsetHeight;
+
+x[0].onscroll = function(){
+ var player = GetPlayer();
+ player.SetVar("num",x[0].scrollTop);
+};
+
+var half1 = height/2;
+var quarter1 = height/4;
+var ended1 = height-15;
+var player1 = GetPlayer();
+player1.SetVar("half",half1);
+player1.SetVar("quarter",quarter1);
+player1.SetVar("ended",ended1);
+player1.SetVar("height",height);
+}
+
+function Script2()
+{
+  var x = document.getElementsByClassName("scrollarea-area");
+x[0].scrollTop = 0;
+var height = x[0].scrollHeight - x[0].offsetHeight;
+
+x[0].onscroll = function(){
+ var player = GetPlayer();
+ player.SetVar("num",x[0].scrollTop);
+};
+
+var half1 = height/2;
+var quarter1 = height/4;
+var ended1 = height-15;
+var player1 = GetPlayer();
+player1.SetVar("half",half1);
+player1.SetVar("quarter",quarter1);
+player1.SetVar("ended",ended1);
+player1.SetVar("height",height);
+}
+
+function Script3()
+{
+  var x = document.getElementsByClassName("scrollarea-area");
+x[0].scrollTop = 0;
+var height = x[0].scrollHeight - x[0].offsetHeight;
+
+x[0].onscroll = function(){
+ var player = GetPlayer();
+ player.SetVar("num",x[0].scrollTop);
+};
+
+var half1 = height/2;
+var quarter1 = height/4;
+var ended1 = height-15;
+var player1 = GetPlayer();
+player1.SetVar("half",half1);
+player1.SetVar("quarter",quarter1);
+player1.SetVar("ended",ended1);
+player1.SetVar("height",height);
+}
+
+function Script4()
+{
+  var x = document.getElementsByClassName("scrollarea-area");
+x[0].scrollTop = 0;
+var height = x[0].scrollHeight - x[0].offsetHeight;
+
+x[0].onscroll = function(){
+ var player = GetPlayer();
+ player.SetVar("num",x[0].scrollTop);
+};
+
+var half1 = height/2;
+var quarter1 = height/4;
+var ended1 = height-15;
+var player1 = GetPlayer();
+player1.SetVar("half",half1);
+player1.SetVar("quarter",quarter1);
+player1.SetVar("ended",ended1);
+player1.SetVar("height",height);
+}
+
+function Script5()
+{
+  var x = document.getElementsByClassName("scrollarea-area");
+x[0].scrollTop = 0;
+var height = x[0].scrollHeight - x[0].offsetHeight;
+
+x[0].onscroll = function(){
+ var player = GetPlayer();
+ player.SetVar("num",x[0].scrollTop);
+};
+
+var half1 = height/2;
+var quarter1 = height/4;
+var ended1 = height-15;
+var player1 = GetPlayer();
+player1.SetVar("half",half1);
+player1.SetVar("quarter",quarter1);
+player1.SetVar("ended",ended1);
+player1.SetVar("height",height);
+}
+
+function Script6()
+{
+  var x = document.getElementsByClassName("scrollarea-area");
+x[0].scrollTop = 0;
+var height = x[0].scrollHeight - x[0].offsetHeight;
+
+x[0].onscroll = function(){
+ var player = GetPlayer();
+ player.SetVar("num",x[0].scrollTop);
+};
+
+var half1 = height/2;
+var quarter1 = height/4;
+var ended1 = height-15;
+var player1 = GetPlayer();
+player1.SetVar("half",half1);
+player1.SetVar("quarter",quarter1);
+player1.SetVar("ended",ended1);
+player1.SetVar("height",height);
+}
+
+function Script7()
+{
+  var x = document.getElementsByClassName("scrollarea-area");
+x[0].scrollTop = 0;
+var height = x[0].scrollHeight - x[0].offsetHeight;
+
+x[0].onscroll = function(){
+ var player = GetPlayer();
+ player.SetVar("num",x[0].scrollTop);
+};
+
+var half1 = height/2;
+var quarter1 = height/4;
+var ended1 = height-15;
+var player1 = GetPlayer();
+player1.SetVar("half",half1);
+player1.SetVar("quarter",quarter1);
+player1.SetVar("ended",ended1);
+player1.SetVar("height",height);
+}
+
+function Script8()
+{
+  var x = document.getElementsByClassName("scrollarea-area");
+x[0].scrollTop = 0;
+var height = x[0].scrollHeight - x[0].offsetHeight;
+
+x[0].onscroll = function(){
+ var player = GetPlayer();
+ player.SetVar("num",x[0].scrollTop);
+};
+
+var half1 = height/2;
+var quarter1 = height/4;
+var ended1 = height-15;
+var player1 = GetPlayer();
+player1.SetVar("half",half1);
+player1.SetVar("quarter",quarter1);
+player1.SetVar("ended",ended1);
+player1.SetVar("height",height);
+}
+
+function Script9()
+{
+  var x = document.getElementsByClassName("scrollarea-area");
+x[0].scrollTop = 0;
+var height = x[0].scrollHeight - x[0].offsetHeight;
+
+x[0].onscroll = function(){
+ var player = GetPlayer();
+ player.SetVar("num",x[0].scrollTop);
+};
+
+var half1 = height/2;
+var quarter1 = height/4;
+var ended1 = height-15;
+var player1 = GetPlayer();
+player1.SetVar("half",half1);
+player1.SetVar("quarter",quarter1);
+player1.SetVar("ended",ended1);
+player1.SetVar("height",height);
+}
+
+function Script10()
+{
+  var x = document.getElementsByClassName("scrollarea-area");
+x[0].scrollTop = 0;
+var height = x[0].scrollHeight - x[0].offsetHeight;
+
+x[0].onscroll = function(){
+ var player = GetPlayer();
+ player.SetVar("num",x[0].scrollTop);
+};
+
+var half1 = height/2;
+var quarter1 = height/4;
+var ended1 = height-15;
+var player1 = GetPlayer();
+player1.SetVar("half",half1);
+player1.SetVar("quarter",quarter1);
+player1.SetVar("ended",ended1);
+player1.SetVar("height",height);
+}
+
